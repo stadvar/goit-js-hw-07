d995f259c7c74ac51af4e4eb8815ec1790ef0c28
stadvar <stadvar@gmail.com>
2020-09-17 15:51:07 +0300

    Update task-6.js

--- scripts/task-6.js
+++ scripts/task-6.js
@@ -2,7 +2,7 @@ const inputRef = document.querySelector('#validation-input');
 const inputCount = Number(inputRef.dataset.length);
 
 inputRef.addEventListener('change', () => {
-  if (inputRef.value.length < inputCount) {
+  if (inputRef.value.length !== inputCount) {
     inputRef.classList.remove('valid');
     inputRef.classList.add('invalid');
   } else {
